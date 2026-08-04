0b7246f1b8ce4badf66a2d58e2ab214301b31eeb
Priyanshu Shishodiya <pratiyushshishodiya@gmail.com>
2026-08-04 15:39:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# this main.py file is for backend testing before actually implementing the streamlit code
 import uuid
 
 from langchain_core.messages import HumanMessage
@@ -11,24 +12,36 @@ if __name__ == "__main__":
             "thread_id": user_id,
         }
     }
-    user_input = input("enter travel request:")
+    while(True):
+        user_input = input("enter travel request:")
 
-    result = app.invoke(
-        {
-            "messages": [
-                HumanMessage(content=user_input)
-            ],
-            "user_query": user_input,
-            "flight_result": "",
-            "hotel_results": "",
-            "weather_result": "",
-            "itinerary": "",
-            "final_result": "",
-            "evaluation": {},
-            "llm_calls": 0,
-            "max_iteration": 3,
-            "revision_count": 0
-        },
-        config=config
-    )
-    print(result["messages"][-1].content)
+        result = app.invoke(
+            {
+                "messages": [
+                    HumanMessage(content=user_input)
+                ],
+                "user_query": user_input,
+                "flight_result": "",
+                "hotel_results": "",
+                "weather_result": "",
+                "itinerary": "",
+                "final_result": "",
+                "evaluation": {},
+                "llm_calls": 0,
+                "max_iteration": 3,
+                "revision_count": 0
+            },
+            config=config
+        )
+        from agents.input_guardrail import InputGuardrail  
+
+        last_msg = result["messages"][-1]
+
+        if isinstance(last_msg, InputGuardrail):
+            print(last_msg.reason)
+        else:
+            print(last_msg.content)
+
+        exit=input("do you want to exit:(y/n)")
+        if(exit=="y"):
+            break
